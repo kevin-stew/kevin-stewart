7445dfd3602bc7f157806c76bc9fa4f6300667f4
Kevin Stewart <94493923+kevin-stew@users.noreply.github.com>
2022-06-30 16:46:30 -0500

    removal of send.py, comment out email code

--- app/site/routes.py
+++ app/site/routes.py
@@ -26,17 +26,17 @@ def contact():
     form = ContactForm()
 
     if request.method == 'POST':
-        msg = EmailMessage()
-        msg.set_content(form.message.data)
-        msg['Subject'] = f"{form.name.data} | {form.email.data}"
-        msg['To'] = Config.MAIL_SENDTO
-        # msg['From'] = form.email.data
+        # msg = EmailMessage()
+        # msg.set_content(form.message.data)
+        # msg['Subject'] = f"{form.name.data} | {form.email.data}"
+        # msg['To'] = Config.MAIL_SENDTO
+        # # msg['From'] = form.email.data
 
-        server = smtplib.SMTP(Config.MAIL_SERVER, Config.MAIL_PORT)
-        server.starttls()  #function that 'starts' the server
-        server.login(Config.MAIL_USERNAME, Config.MAIL_PASSWORD)
-        server.sendmail(Config.MAIL_USERNAME, Config.MAIL_SENDTO, msg.as_string())
-        server.quit()
+        # server = smtplib.SMTP(Config.MAIL_SERVER, Config.MAIL_PORT)
+        # server.starttls()  #function that 'starts' the server
+        # server.login(Config.MAIL_USERNAME, Config.MAIL_PASSWORD)
+        # server.sendmail(Config.MAIL_USERNAME, Config.MAIL_SENDTO, msg.as_string())
+        # server.quit()
 
         flash(f"Thanks for your message! I will get back to you as soon as I can :)")
         return render_template("index.html")
