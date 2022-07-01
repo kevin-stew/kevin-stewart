7e67583ca594360329580840fe6dad48660e1bc5
Kevin Stewart <94493923+kevin-stew@users.noreply.github.com>
2022-06-30 20:34:24 -0500

    220630 with functioning mail form code

--- config.py
+++ config.py
@@ -8,4 +8,13 @@ class Config():
     Using Environment variables where available otherwise
     create the config variable if not done already.
     """
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Random string'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    #mail credentials
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') 
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') 
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SENDTO = os.environ.get('MAIL_SENDTO')
