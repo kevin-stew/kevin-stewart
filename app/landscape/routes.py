00092775de3f7ce7390b214d33bc3ca33392f84c
Kevin Stewart <94493923+kevin-stew@users.noreply.github.com>
2023-07-04 18:08:47 -0500

    landscape page reorganization, new landscape projects

--- app/landscape/routes.py
+++ app/landscape/routes.py
@@ -17,3 +17,19 @@ def aml():
 @landscape.route('/stewart-residence')
 def stewart_residence():
     return render_template('stewart_residence.html')
+
+@landscape.route('/streamside-residence')
+def streamside_residence():
+    return render_template('streamside_residence.html')
+
+@landscape.route('/park-at-the-mart')
+def park_at_the_mart():
+    return render_template('park_at_the_mart.html')
+
+@landscape.route('/lakeshore-east')
+def lakeshore_east():
+    return render_template('lakeshore_east.html')
+
+@landscape.route('/raised-beds')
+def raised_beds():
+    return render_template('raised_beds.html')
